dd55b762a09c7d57dcc7ecb9fd17597417f05389
menneck <zeeeee.peeeeetryyy@gmail.com>
2023-03-10 14:23:14 -0300

    remove extends ListenerAdapter

--- src/main/java/org/javabot/DiscordBot.java
+++ src/main/java/org/javabot/DiscordBot.java
@@ -4,13 +4,12 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.javabot.Core.CommandListener;
 import org.javabot.Listeners.ReadyListener;
 import org.javabot.Utils.Util;
 
-public class DiscordBot extends ListenerAdapter {
+public class DiscordBot {
 
     public static void main(String[] args) {
 
